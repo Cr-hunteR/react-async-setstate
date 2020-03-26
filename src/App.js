a450914b319ffb17530a206611ded59711389579
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-03-27 00:40:39 +0530

    title updated

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ class App extends React.Component {
       <div className="App">
         <div className="row justify-content-md-center align-items-center row-container">
           <div className="col-md-8">
-            <h1>setState() in React</h1>
+            <h1>React Live Input Reader</h1>
             <div className="card">
               <form>
                 <div className="form-group">
                   <input type="text" className="form-control" id="formGroupExampleInput" onChange={(change)=>{
                     this.setState({inputCatcher:change.target.value});
-                    console.log(this.state.inputCatcher);
+                    console.log(this.state.inputCatcher);//Console output
                     }}>
                   </input>
-                  <p className="para">{this.state.inputCatcher}</p>
+                  <p className="para">{this.state.inputCatcher}</p>{/* Main output */}
                 </div>
               </form>
             </div>
